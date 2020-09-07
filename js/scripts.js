0e9efb7349d68ff42ef891874940dec44a117a0c
DanielFernandoYepezVelez <danipez.02@gmail.com>
2020-09-07 16:53:06 -0500

    Termiando El Proyecto de MiViaje.com

---
+++ js/scripts.js
@@ -0,0 +1,48 @@
+let i = 0;
+let tiempo = 0;
+
+const imagenes = ['arriba2.jpg', 'arriba.jpg'];
+const footer = document.querySelector('#footer');
+const imagenHero = document.querySelector('.hero');
+const btnFlotante = document.querySelector('.btn__flotante');
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    setInterval(() => {
+        imagenHero.style.backgroundPositionY = '-' + tiempo + 'px';
+
+        if (tiempo > 40) {
+            tiempo = 0;
+
+            imagenHero.style.backgroundImage = `url(../img/${imagenes[i]})`;
+
+            if (i === (imagenes.length - 1)) {
+                i = 0;
+            } else {
+                i++;
+                console.log(i);
+            }
+        }
+
+        tiempo++;
+        // console.log(tiempo);
+    }, 100);
+
+});
+
+btnFlotante.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    if (footer.classList.contains('activo')) {
+        footer.classList.remove('activo');
+        btnFlotante.classList.remove('activo');
+
+        btnFlotante.textContent = 'Idioma Y Moneda';
+    } else {
+        footer.classList.add('activo');
+        btnFlotante.classList.add('activo');
+
+        btnFlotante.textContent = 'X Cerrar';
+    }
+});
